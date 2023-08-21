7ef400a2892c04b634ac9f9ee49c8ec9a454a7b6
Mohammed-Sanih <129580388+Mohammed-Sanih@users.noreply.github.com>
2023-08-21 14:24:27 +0530

    Java Program
    Java Program

---
+++ MultipleOfTenChecker/src/com/kodnest/Main.java
@@ -0,0 +1,26 @@
+package com.kodnest;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void checkMultipleOfTen(int n)
+	{
+		if(n%10==0) {
+			System.out.println("The number is a multiple of 10");
+		}
+		else
+		{
+			System.out.println("The number is a not multiple of 10");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		checkMultipleOfTen(n);
+
+	}
+
+}
